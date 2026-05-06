b36605e211a3521a4847d86468fa9d611ca34771
Viniciusxz <viniciusribeirolinsr@gmail.com>
2026-05-06 17:47:02 -0300

    Adicionando uma seed teste e populando o banco.

---
+++ src/seeders/20260506204226-demo-cliente.js
@@ -0,0 +1,20 @@
+export default {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('clientes', [
+      {
+        nome: 'Cliente Teste',
+        cpf: '12345678900',
+        email: 'teste@email.com',
+        senha: '123456',
+        telefone: '81999999999',
+        endereco: 'Rua Teste, 123',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('clientes', null, {});
+  }
+};
