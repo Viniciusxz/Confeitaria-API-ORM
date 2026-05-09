2f123b091c6651b698f0683bc4d4ce3f034fe22c
Viniciusxz <viniciusribeirolinsr@gmail.com>
2026-05-09 18:39:00 -0300

    Criação dos Controllers e Rotas

---
+++ src/controllers/ClienteController.js
@@ -0,0 +1,11 @@
+class ClienteController { // Papel do controller é receber o que vem da rota e mandar pro modelo, ou seja, responsavel por gerenciar as requisições e respostas.
+    static async pegaTodas (req, res) {
+        try {
+            // acessar o modelo
+        } catch (erro) {
+            // erro
+        }
+    }
+}
+
+export default ClienteController;
